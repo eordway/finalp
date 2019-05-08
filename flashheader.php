97c0c780e393a32c09c32c441a04ae29830b4d0c
eordway <47196262+eordway@users.noreply.github.com>
2019-05-08 12:25:58 -0400

    Create flashheader.php

---
+++ flashheader.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?php
+
+	
+?>
+<ul id= "menu-bar">
+
+<li><a href= "flashhome.php">Home</a></li>
+<li><a href= "flashloginpage.php">Login</a></li>
+<li><a href= "search.php">Search database</a></li>
+<li><a href= "newgame.php">Add new game</a></li>
+<li><a href= "logout.php">Logout</a></li>
+
+
+
+</ul>
+</html> 
